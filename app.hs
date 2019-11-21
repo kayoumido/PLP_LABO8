aa7db643ff3c12f8ea446bb39353b02e3f88b7cb
KurohanaJuri <chau.kot@heig-vd.ch>
2019-11-21 17:58:45 +0100

    Add eval function

--- app.hs
+++ app.hs
@@ -3,29 +3,58 @@ module Lab8 where
 
 type Op = Char
 type Name = [Char]
-data Exp = Cnst Int | Bin Op Exp Exp  | Un Op Exp | Cond Exp Exp Exp | Var Name | Let Name Exp Exp  deriving (Show)
+data Exp = Cnst Int | Bin Op Exp Exp  | Un Op Exp | Cond Exp Exp Exp |
+     Var Name | Let Name Exp Exp | App Name [Exp] | Def Name [Name] Exp deriving (Show)
 
-value v [] = error $ v ++ " not found (value)"
-value v ((var, val):env) =
-    if v == var then val else value v env
+value name (vars, _) = value' name vars
+    where value' name [] = 0
+          value' name ((var, val): vars) = 
+            if name == var then val else value' name vars
+
+extract name (_, funcs) = extract' name funcs
+    where extract' name [] = error $ "Undefined function: " ++ name
+          extract' name ((func, vars, body):funcs) = 
+            if name == func then (vars, body) else extract' name funcs
+
+bool op x y  = if (op x y ) then 1 else 0
+
+expand env [] [] = env
+expand env (v:vs) (x:xs) = ((v,eval x env):vars, funcs)
+    where (vars, funcs) = expand env vs xs
+
 
 eval (Cnst n) _         = n
 eval (Var v) env        = value v env
 
 eval (Bin '+' a b) env  = (eval a env) + (eval b env)
-eval (Bin '<' a b) env  = if (eval a env) < (eval b env) then 1 else 0
+eval (Bin '-' a b) env  = (eval a env) - (eval b env)
+eval (Bin '<' a b) env  = bool (<) (eval a env) (eval b env)
 
 eval (Un '-' a) env     = - eval a env
 
 eval (Cond p ifTrue ifFalse) env
-    | eval p  env == 1  = eval ifTrue env
-    | otherwise         = eval ifFalse env
+    | eval p env < 1  = eval ifFalse env
+    | otherwise       = eval ifTrue env
 
-eval (Let n x y) env = eval y ((n, eval x env):env)
+eval (Let n x y) env@(vars, funcs) = eval y ((n, eval x env):vars, funcs)
 
-condOk = (Bin '+' (Cnst 1) (Cnst 5))
-condNotOk = (Bin '+' (Cnst 5) (Cnst 5)) 
+eval (App func xs ) env = eval body $ expand env vars xs where (vars, body) = extract func env
+
+
+lexer [] = []
+ 
 plusPetit = (Bin '<' (Cnst 5) (Cnst 2))
 
-env :: [(Name, Int)]
-env = [("a", 1), ("b", 2), ("c", 3)]
+funcs = 
+    [
+        ("succ", ["N"], Bin '+' (Var "N") (Cnst 1)),
+        ("pred", ["N"], Bin '-' (Var "N") (Cnst 1)),
+        ("somme", ["N"], 
+            Cond 
+                (Var "N") 
+                (Bin '+' (Var "N") (App "somme" [App "pred" [Var "N"]]))
+                (Cnst 0))
+    ]
+
+env :: ([(Name, Int)], [(Name, [Name], Exp)])
+env = ([("a", 1), ("b", 2), ("c", 3)], funcs)
