dd4d8e36f535b1257a323c3fa17e6342cbe4b73e
KurohanaJuri <chau.kot@heig-vd.ch>
2019-11-27 20:43:40 +0100

    Co-authored-by: Dodoross C'est de la merde mais ca fonctionne ou pas

--- app.hs
+++ app.hs
@@ -1,9 +1,10 @@
 module Lab8 where
 
+import Data.Char
 
-type Op = Char
+type Op = [Char]
 type Name = [Char]
-data Exp = Cnst Int | Bin Op Exp Exp  | Un Op Exp | Cond Exp Exp Exp |
+data Exp = Cnst Int | Bin Op Exp Exp | Un Op Exp | Cond Exp Exp Exp |
      Var Name | Let Name Exp Exp | App Name [Exp] | Def Name [Name] Exp deriving (Show)
 
 value name (vars, _) = value' name vars
@@ -22,15 +23,15 @@ expand env [] [] = env
 expand env (v:vs) (x:xs) = ((v,eval x env):vars, funcs)
     where (vars, funcs) = expand env vs xs
 
-
 eval (Cnst n) _         = n
 eval (Var v) env        = value v env
 
-eval (Bin '+' a b) env  = (eval a env) + (eval b env)
-eval (Bin '-' a b) env  = (eval a env) - (eval b env)
-eval (Bin '<' a b) env  = bool (<) (eval a env) (eval b env)
+eval (Bin "+" a b) env  = (eval a env) + (eval b env)
+eval (Bin "-" a b) env  = (eval a env) - (eval b env)
+eval (Bin "<" a b) env  = bool (<) (eval a env) (eval b env)
+eval (Bin "<=" a b) env  = bool (<=) (eval a env) (eval b env)
 
-eval (Un '-' a) env     = - eval a env
+eval (Un "-" a) env     = - eval a env
 
 eval (Cond p ifTrue ifFalse) env
     | eval p env < 1  = eval ifFalse env
@@ -40,19 +41,36 @@ eval (Let n x y) env@(vars, funcs) = eval y ((n, eval x env):vars, funcs)
 
 eval (App func xs ) env = eval body $ expand env vars xs where (vars, body) = extract func env
 
-
 lexer [] = []
- 
-plusPetit = (Bin '<' (Cnst 5) (Cnst 2))
+lexer (c:cs) 
+    | isSpace c = lexer cs
+    | isAlpha c = lexSym (c:cs)
+    | isDigit c = lexInt (c:cs)
+
+lexer ('<':'=':cs) = "<=": lexer cs -- <- C MOA KA FÉ (DODO)
+lexer ('<':cs) = "<": lexer cs
+lexer ('-':cs) = "-": lexer cs
+lexer ('+':cs) = "+": lexer cs
+lexer ('=':cs) = "=": lexer cs
+
+
+lexInt cs = int : lexer rest
+    where (int, rest) = span isDigit cs
+
+lexSym cs = symbol : lexer rest
+    where (symbol,rest) = span isAlpha cs
+
+-- TEST --
+plusPetit = (Bin "<" (Cnst 5) (Cnst 2))
 
 funcs = 
     [
-        ("succ", ["N"], Bin '+' (Var "N") (Cnst 1)),
-        ("pred", ["N"], Bin '-' (Var "N") (Cnst 1)),
+        ("succ", ["N"], Bin "+" (Var "N") (Cnst 1)),
+        ("pred", ["N"], Bin "-" (Var "N") (Cnst 1)),
         ("somme", ["N"], 
             Cond 
                 (Var "N") 
-                (Bin '+' (Var "N") (App "somme" [App "pred" [Var "N"]]))
+                (Bin "+" (Var "N") (App "somme" [App "pred" [Var "N"]]))
                 (Cnst 0))
     ]
 
